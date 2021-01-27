e8273cff6cc716894855cc4277e18dc568d06f5d
Eiks <eiks_art@outlook.com>
2021-01-27 10:03:19 -0400

    clearing

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-const puppet = require('puppeteer');
-const fs = require('fs');
+const puppet = require("puppeteer");
+const fs = require("fs");
 
-(async ()=> {
-    let chanteurs = [];
-    const browser = await puppet.launch();
-    let annee = 1955 ;
-    const page = await browser.newPage();
-    await page.on('console', consoleObj => console.log(consoleObj.text()));
+(async () => {
+  const browser = await puppet.launch();
+  let annee = 1955;
+  const page = await browser.newPage();
+  await page.on("console", (consoleObj) => console.log(consoleObj.text()));
 
-    for (annee ; annee <= 1958; annee++ ){
-        await page.goto(`https://fr.wikipedia.org/wiki/Liste_des_titres_musicaux_num%C3%A9ro_un_en_France_en_${annee}`)
-        const result = await page.evaluate((annee) => {
-                let titres = document.querySelectorAll(".wikitable tr")
-                console.log(annee)
+  for (annee; annee <= 1956; annee++) {
+    await page.goto(
+      `https://fr.wikipedia.org/wiki/Liste_des_titres_musicaux_num%C3%A9ro_un_en_France_en_${annee}`
+    );
+    const result = await page.evaluate((annee) => {
+      let titres = document.querySelectorAll(".wikitable tr");
 
-                let chMap = Array.prototype.map.call(titres, function (titre) {
+      let chMap = Array.prototype.map.call(
+        titres,
+        function (titre) {
+          let title = titre.querySelector("td:nth-child(0n+4)");
+          let author = titre.querySelector("td:nth-child(0n+3)");
+          if (title != null) {
+            return {
+              auteur: author.innerText,
+              titre: title.innerText,
+              annee: annee,
+            };
+          }
+        },
+        annee
+      );
+      return chMap.filter((element) => element != null);
+    }, annee);
 
-                    let title = titre.querySelector("td:nth-child(0n+4)")
-                    let author = titre.querySelector("td:nth-child(0n+3)")
-                    if (title != null) {
+    await console.log(result);
+    fs.writeFile("./singles.json", JSON.stringify(result), function (err) {});
+  }
 
-                        return {auteur: author.textContent, titre: title.textContent, annee: annee};
-
-                    }
-
-                }, annee)
-                return chMap.filter(element => element != null)
-            },
-            annee
-        )
-
-        await chanteurs.push(result);
-        await console.log(result);
-        fs.writeFile('./test.txt', (JSON.stringify(result)) , function (err) {})
-
-
-
-    }
-
-
-    await browser.close();
+  await browser.close();
 })();
